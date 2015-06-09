c77e4e3c17b637b54c79b2b554bfaa3926695b2f
unknown <pbutton@zugwks022.trading.imc.intra>
2015-06-09 17:05:00 +0200

    Preparing for Heroku

--- coffeed/settings.py
+++ coffeed/settings.py
@@ -81,6 +81,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 
 # Internationalization
@@ -104,4 +107,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=(
     os.path.join(MAIN_DIR, 'static'),
     )
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT =  'staticfiles'
